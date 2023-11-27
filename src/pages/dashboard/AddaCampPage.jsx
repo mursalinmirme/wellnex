fa2e47b7c2eb78c691a515792a96abe1133a45df
mursalinmirme <mursalinmir02@gmail.com>
2023-11-27 14:03:19 +0600

    I have make organizers add a new camp added system dynamically. When a user will submit a camp data it will store in database also it will visible in in my website

--- src/pages/dashboard/AddaCampPage.jsx
+++ src/pages/dashboard/AddaCampPage.jsx
@@ -5,27 +5,84 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { setHours, setMinutes } from 'date-fns';
+import axios from "axios";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+import useAuth from "../../hooks/useAuth";
 const AddaCampPage = () => {
+    const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const [errorMsg, setErrorMsg] = useState('');
     const [startDate, setStartDate] = useState(
         // setHours(setMinutes(new Date(), 30), 16),
       );
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm()
 
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    setErrorMsg('')
+    const camp_name = data.camp_name;
+    const camp_fees = data.camp_fees;
+    const scheduled_date_time = startDate;
+    const venue_location = data.venue_location;
+    const specialized_services = data.specilize_services;
+    const healthcare_professionals = data.health_professionals;
+    const target_audience = data.target_audience;
+    const description = data.description;
+    const camp_image = data.camp_image[0];
+
+    if(!startDate){
+        setErrorMsg('Select shedule time');
+        return
+    }
+
+    const formData = new FormData();
+    formData.append("image", camp_image);
+    const uploadImage = await axios.post(
+      `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_imageBB_API}`,
+      formData
+    );
+    const uploadImgResp = await uploadImage.data;
+
+
+    const uploadImagellink = uploadImgResp.data.display_url;
+
+    const newCamp = {camp_name, camp_fees, scheduled_date_time, venue_location, specialized_services:[specialized_services], healthcare_professionals:[healthcare_professionals], target_audience, description, image:uploadImagellink, campOwnerEmail: user?.email, campOwnerName: user?.displayName};
+    console.log(newCamp);
+
+    if(uploadImgResp.success){
+        axiosSecure.post('/add-a-camp', newCamp)
+        .then(res => {
+            console.log(res.data);
+            if(res.data.acknowledged){
+                toast.success('Your camp successfully added!')
+            }
+        })
+        .catch(error => {
+            console.log(error.message);
+    })
+    }
+
+
+    console.log(uploadImgResp);
+
+
+
+
+
+
+
   }
 
 
 
   return (
 
-    <Paper sx={{padding: '20px', }}>
+    <Paper sx={{padding: '20px'}}>
         <Box>
         <Typography textAlign={'center'} fontWeight={'600'} fontSize={'28px'} mt={'30px'}>Add a New Camp</Typography>
         <label htmlFor=""></label>
@@ -57,40 +114,49 @@ const AddaCampPage = () => {
       ]}
       dateFormat="MMMM d, yyyy h:mm aa"
     />
+    {
+        errorMsg && <span style={{color: 'red'}}>{errorMsg}</span>
+    }
       </Box>
       </Box>
 
-      <Box mt={'20px'}>
+      <Box gap={'20px'} display={'flex'}>
+      <Box flex={1} mt={'20px'}>
+      <label style={{fontWeight: '600'}}>Camp Image</label>
+      <input type="file" style={{display: 'block', width: '100%', padding: '12px', fontSize: '16px', outline: 'none' ,marginTop: '5px', border: '0.1px solid rgb(118, 118, 118)'}} placeholder="Enter your camp name" {...register("camp_image", { required: true })} />
+      {errors.camp_image && <span style={{color: 'red'}}>Select camp Image</span>}
+      </Box>
+
+      <Box flex={1} mt={'20px'}>
       <label style={{fontWeight: '600'}}>Venue Location:</label>
-      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("date_time", { required: true })} />
-      {errors.date_time && <span style={{color: 'red'}}>Enter sheduled date and time</span>}
+      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("venue_location", { required: true })} />
+      {errors.venue_location && <span style={{color: 'red'}}>Enter venue location</span>}
+      </Box>
       </Box>
 
       <Box mt={'20px'}>
       <label style={{fontWeight: '600'}}>Specialized Services Provided:</label>
-      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("camp_name", { required: true })} />
-      {errors.camp_name && <span style={{color: 'red'}}>Enter sheduled date and time</span>}
+      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("specilize_services", { required: true })} />
+      {errors.specilize_services && <span style={{color: 'red'}}>Enter sheduled date and time</span>}
       </Box>
 
       <Box mt={'20px'}>
       <label style={{fontWeight: '600'}}>Healthcare Professionals:</label>
-      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("camp_name", { required: true })} />
-      {errors.camp_name && <span style={{color: 'red'}}>Enter sheduled date and time</span>}
+      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("health_professionals", { required: true })} />
+      {errors.health_professionals && <span style={{color: 'red'}}>Enter Healthcare Professionals</span>}
       </Box>
 
       <Box mt={'20px'}>
       <label style={{fontWeight: '600'}}>Target Audience:</label>
-      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("camp_name", { required: true })} />
-      {errors.camp_name && <span style={{color: 'red'}}>Enter sheduled date and time</span>}
+      <input style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px'}} placeholder="Enter your camp name" {...register("target_audience", { required: true })} />
+      {errors.target_audience && <span style={{color: 'red'}}>Enter target audience</span>}
       </Box>
 
       <Box mt={'20px'}>
       <label style={{fontWeight: '600'}}>Description:</label>
       <textarea style={{display: 'block', width: '100%', padding: '14px', fontSize: '16px', outline: 'none' ,marginTop: '5px', resize: 'none', height: '200px'}} placeholder="Enter your camp description" {...register("description", { required: true })}>
-      {errors.description && <span style={{color: 'red'}}>Enter sheduled date and time</span>}
-
       </textarea>
-      
+      {errors.description && <span style={{color: 'red'}}>Enter your camp description</span>}
 
       </Box>
 
